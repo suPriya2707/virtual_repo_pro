08cdb4c510da21b7bc578ddc748d37f0b5a5d39a
Supriya <supriya.jarugu@gmail.com>
2021-09-29 15:05:07 +0000

    search feature

--- src/com/Fileop.java
+++ src/com/Fileop.java
@@ -2,36 +2,11 @@ package com;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Fileop {
-	public static void listFilesInDirectory(String path, int indentationCount) {
-		File dir = new File(path);
-        File[] files = dir.listFiles();
-
-        if (files != null && files.length > 0) {
-            for (File file : files) {
-
-            	System.out.print(" "+(indentationCount*2));
-
-
-            	if (file.isDirectory()) {  
-            		System.out.println("`-- " + file.getName());
-
-            		// Recursively indent and display the files
-                	listFilesInDirectory(file.getAbsolutePath(), indentationCount+1);
-                }  else {
-                	System.out.println("|-- " + file.getName());
-                }
-            }
-        } else {
-        	System.out.print(" "+(indentationCount*2));
-        	System.out.println("|-- Empty Directory");
-        }
-        System.out.println();
-	}
-}
 	//public static void listFilesInDirectory(String path, int indentationCount) {
 		public static List<String> listFilesInDirectory(String path, int indentationCount, List<String> fileListNames) {
 			File dir = new File(path);
@@ -66,7 +41,106 @@ public class Fileop {
 	        }
 	        System.out.println();
 	        return fileListNames;
-		}
+	public static void displayAllFiles(String path) {
+		// All required files and folders inside "main" folder relative to current
+		// folder
+		System.out.println("Displaying all files with directory structure in ascending order\n");
 
+		// listFilesInDirectory displays files along with folder structure
+		List<String> filesListNames = FileOperations.listFilesInDirectory(path, 0, new ArrayList<String>());
+
+		System.out.println("Displaying all files in ascending order\n");
+		Collections.sort(filesListNames);
+
+		filesListNames.stream().forEach(System.out::println);
+	}
+
+	public static List<String> listFilesInDirectory(String path, int indentationCount, List<String> fileListNames) {
+		File dir = new File(path);
+     /*   File[] files = dir.listFiles();
+        List<File> filesList = Arrays.asList(files);
+        Collections.sort(filesList);
+        if (files != null && files.length > 0) {
+            for (File file : filesList) {
+            	System.out.print(" ".repeat(indentationCount*2));
+            	if (file.isDirectory()) {  
+            		System.out.println("`-- " + file.getName());
+            		// Recursively indent and display the files
+            		fileListNames.add(file.getName());
+                	listFilesInDirectory(file.getAbsolutePath(), indentationCount+1, fileListNames);
+                }  else {
+                	System.out.println("|-- " + file.getName());
+                	fileListNames.add(file.getName());
+                }
+            }
+        } else {
+        	System.out.print(" ".repeat(indentationCount*2));
+        	System.out.println("|-- Empty Directory");
+        }
+        System.out.println();
+        return fileListNames;   */
+
+
+        File[] files = dir.listFiles();
+		List<File> filesList = Arrays.asList(files);
+
+		Collections.sort(filesList);
+
+		if (files != null && files.length > 0) {
+			for (File file : filesList) {
+
+				System.out.print(" "+(indentationCount * 2));
+
+				if (file.isDirectory()) {
+					System.out.println("`-- " + file.getName());
+
+					// Recursively indent and display the files
+					fileListNames.add(file.getName());
+					listFilesInDirectory(file.getAbsolutePath(), indentationCount + 1, fileListNames);
+				} else {
+					System.out.println("|-- " + file.getName());
+					fileListNames.add(file.getName());
+				}
+			}
+		} else {
+			System.out.print(" "+(indentationCount * 2));
+			System.out.println("|-- Empty Directory");
+		}
+		System.out.println();
+		return fileListNames;
+	}
+
+	public static void displayFileLocations(String fileName, String path) {
+		List<String> fileListNames = new ArrayList<>();
+		FileOperations.searchFileRecursively(path, fileName, fileListNames);
+
+		if (fileListNames.isEmpty()) {
+			System.out.println("\n\n***** Couldn't find any file with given file name \"" + fileName + "\" *****\n\n");
+		} else {
+			System.out.println("\n\nFound file at below location(s):");
+			fileListNames.stream().forEach(System.out::println);
+		}
+	}
+
+	public static void searchFileRecursively(String path, String fileName, List<String> fileListNames) {
+		File dir = new File(path);
+		File[] files = dir.listFiles();
+		List<File> filesList = Arrays.asList(files);
+
+		if (files != null && files.length > 0) {
+			for (File file : filesList) {
+
+				if (file.getName().startsWith(fileName)) {
+					fileListNames.add(file.getAbsolutePath());
+				}
+
+				// Need to search in directories separately to ensure all files of required fileName are searched
+				if (file.isDirectory()) {
+					searchFileRecursively(file.getAbsolutePath(), fileName, fileListNames);
+				}
+			}
+		}
+	}
 
 	} 
+} 
--- src/com/Main.java
+++ src/com/Main.java
@@ -2,59 +2,86 @@ package com;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args){
-		
-		MenuOP.printWelcome("Virtual Pro", "Supriya");
+		//Welcome.printWelcome("Virtual Key Repository", "Pavithra");
+		MenuOP.printWelcome("Virtual Key Repository", "Pavithra");
 		handleWelcomeScreenInput();
 	}
 	private static void handleWelcomeScreenInput() {
 		boolean running = true;
 		Scanner sc = new Scanner(System.in);
 
-		
 		do {
 			MenuOP.displayMenu();
 			int input = sc.nextInt();
+			//MenuOptions.displayMenu();
+			//int input = sc.nextInt();
+			try {
+				MenuOP.displayMenu();
+				int input = sc.nextInt();
 
 			switch (input) {
+		/*	switch (input) {
 			case 1:
 				// All required files and folders inside main folder relative to current folder
-				Fileop.listFilesInDirectory("./main", 0);
 				//FileOperations.listFilesInDirectory("./main", 0);
 				displayAllFiles();
 				break;
+			displayAllFiles();
+		      break;
 			case 2:
-				MenuOP.displayFileMenuOptions();
+				MenuOptions.displayFileMenuOptions();
 				break;
-
 			case 3:
 				System.out.println("Program exited successfully.");
+		     System.out.println("Program exited successfully.");
 				System.exit(0);
 				break;
 			default:
 				System.out.println("Please select a valid option from above.");
 			}
+			}*/
+				switch (input) {
+				case 1:
+					Fileop.displayAllFiles("./main");
+					break;
+				case 2:
+					MenuOP.handleFileMenuOptions();
+					break;
+				case 3:
+					System.out.println("Program exited successfully.");
+					running = false;
+					sc.close();
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Please select a valid option from above.");
+				}
+			} catch (Exception e) {
+				System.out.println(e.getClass().getName());
+				handleWelcomeScreenInput();
+			} 
 		} while (running == true);
-
 		
 		sc.close();
 
+	/*	sc.close();
 	}
-
 	private static void displayAllFiles() {
 		// All required files and folders inside main folder relative to current folder
 		System.out.println("Displaying all files with directory structure in ascending order\n");
-		List<String> filesListNames = Fileop.listFilesInDirectory("./main", 0, new ArrayList<String>());
-
-	}
-
+		List<String> filesListNames = FileOperations.listFilesInDirectory("./main", 0, new ArrayList<String>());
 		System.out.println("Displaying all files in ascending order\n");
 		Collections.sort(filesListNames);
-
 		filesListNames.stream()
 				.forEach(System.out::println);
+				.forEach(System.out::println);    */
 	}
 }
+} 
